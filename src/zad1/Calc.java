6c02efad621431cddb521f7c5dc9caea0bef78c3
Jakub Przybylski <jakubp02.10@gmail.com>
2024-04-22 10:36:33 +0200

    Zad1. division fixed.

--- src/zad1/Calc.java
+++ src/zad1/Calc.java
@@ -8,22 +8,22 @@ package zad1;
 
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BinaryOperator;
 
 public class Calc {
-
-    private final Map<String, BinaryOperator<BigDecimal>> operations = new HashMap<>();
+    private final Map<String, BinaryOperator<BigDecimal>> operations;
     public Calc(){
+        operations = new HashMap<>();
         operations.put("+", BigDecimal::add);
         operations.put("-", BigDecimal::subtract);
         operations.put("*", BigDecimal::multiply);
-        operations.put("/", BigDecimal::divide);
+        operations.put("/", (v1, v2) -> v1.divide(v2, 6, RoundingMode.HALF_UP));
     }
-    public String doCalc(String cmd) {
-        cmd = cmd.replaceAll("\\s+","");
-        String[] parts = cmd.split("");
+    public String doCalc(String input) {
+        String[] parts = input.split("\\s");
         try{
             BigDecimal a = new BigDecimal(parts[0]);
             BigDecimal b = new BigDecimal(parts[2]);
@@ -31,7 +31,7 @@ public class Calc {
             BinaryOperator<BigDecimal> operator = operations.get(op);
             return operator.apply(a,b).toString();
         }catch (Exception e){
-            return "Invalid command to calc";
+            return "Invalid command to calc!\n"+e.getMessage();
         }
     }
 }
